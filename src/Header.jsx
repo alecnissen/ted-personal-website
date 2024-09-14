daebfdc3ecc7f87d2278261e5a953bb3b6844d24
alecnissen <alecnissen@yahoo.com>
2024-09-14 19:53:53 -0400

    created basic header content

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header-wrapper'>
+    
+    <div className='header-button-links-container'>
+        <button>Home</button>
+        <button>Biography</button>
+        <button>Publications & Lectures</button>
+        <button>Gallery</button>
+        <button>Contact</button>
+    </div>
+
+
+    </div>
+  )
+}
