96fa15e681d978451c62c63bd242cbbb3ac5cd1c
alecnissen <alecnissen@yahoo.com>
2024-09-15 23:36:57 -0400

    added card section base content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Header"
 import ImgAndDescription from "./ImgAndDescription"
+import ExpertiseCards from "./ExpertiseCards"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <> 
     <Header></Header>
     <ImgAndDescription></ImgAndDescription>
+    <ExpertiseCards></ExpertiseCards>
     </>
   )
 }
---
+++ src/ExpertiseCards.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import indianSvg from './Assets/indian-1-svgrepo-com.svg';
+import civilWarImg from './Assets/lincoln-2-svgrepo-com.svg';
+import militaryImg from './Assets/military-medal-svgrepo-com.svg';
+import vikingImg from './Assets/viking-svgrepo-com.svg';
+
+export default function ExpertiseCards() {
+  return (
+    <div className='expertise-cards-content-wrapper'>
+     
+        <div className='expertise-cards-container'>
+
+            <div className='expertise-card'>
+                <h1>Indian History</h1>
+                
+                <div className='expertise-img-container'>
+                <img src={indianSvg}></img>
+                </div>
+
+            </div>
+
+            <div className='expertise-card'>
+                <h1>Civil War</h1>
+                
+                <div className='expertise-img-container'>
+                <img src={civilWarImg}></img>
+                </div>
+
+            </div>
+
+            <div className='expertise-card'>
+                <h1>Military History</h1>
+                
+                <div className='expertise-img-container'>
+                <img src={militaryImg}></img>
+                </div>
+
+            </div>
+
+            <div className='expertise-card'>
+                <h1>Vikings</h1>
+                
+                <div className='expertise-img-container'>
+                <img src={vikingImg}></img>
+                </div>
+
+            </div>
+
+
+
+        </div>
+
+
+    </div>
+  )
+}
