a99e47c7986761e1dd778428d50b7a178554e66b
alecnissen <alecnissen@yahoo.com>
2024-11-19 16:33:50 -0500

    featured lectures section complete

--- src/FeaturedLectures.jsx
+++ src/FeaturedLectures.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import img1 from './Assets/prof-ted-lecture-photo.png';
 import img2 from './Assets/prof-ted-lecture-photo-2.png';
+import img3 from './Assets/prof-ted-lecture-photo-3.png';
+import img4 from './Assets/prof-ted-lecture-photo-4.png';
 
 export default function FeaturedLectures() {
   return (
@@ -17,12 +19,30 @@ export default function FeaturedLectures() {
         </div>
 
         <div className="featured-lecture-container">
-          <a href="https://www.youtube.com/watch?v=PElhjHIbyd0" target="_blank">
+          <a href="https://www.youtube.com/watch?v=qh3JLgjEKkk&t=148s" target="_blank">
             <h1>Illinois, Chicago, and the Civil War</h1>
           </a>
 
           <img src={img2} className="lecture-img-2-styles"></img>
         </div>
+
+
+        <div className="featured-lecture-container">
+          <a href="https://www.youtube.com/watch?v=_7IKl0tDXEA&t=161s" target="_blank">
+            <h1>Calumet: The Region & The Anthropocene</h1>
+          </a>
+
+          <img src={img3} className="lecture-img-2-styles"></img>
+        </div>
+
+
+       
+
+
+        
+
+
+
       </div>
     </div>
   );
