1fa7ba7aaf62b1d1ad30bee3c2b89595a8efed3e
alecnissen <alecnissen@yahoo.com>
2024-11-23 18:05:05 -0500

    edited lecture cards

--- src/FeaturedLectures.jsx
+++ src/FeaturedLectures.jsx
@@ -13,29 +13,53 @@ export default function FeaturedLectures() {
       <h1>Featured Lectures</h1>
 
       <div className="featured-lectures-video-container">
-        <div className="featured-lecture-container">
+
+
+        {/* <div className="featured-lecture-container">
           <a href="https://www.youtube.com/watch?v=PElhjHIbyd0" target="_blank">
             <h1>The Civil War as an Indian War</h1>
           </a>
 
           <img src={img1} className="lecture-img-1-styles"></img>
-        </div>
+        </div> */}
 
-        <div className="featured-lecture-container">
-          <a href="https://www.youtube.com/watch?v=qh3JLgjEKkk&t=148s" target="_blank">
+        <Card className="featured-lecture-container">
+        <a href="https://www.youtube.com/watch?v=PElhjHIbyd0" target="_blank">
+            <h1>The Civil War as an Indian War</h1>
+          </a>
+          <img src={img1} className="lecture-img-2-styles"></img>
+
+          <Card.Body>
+            <p className='lecture-description-styles'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat deleniti harum repellendus doloremque maxime sit ut, explicabo architecto rem commodi, a suscipit magnam, numquam odit asperiores ex fuga. Fugit, suscipit.
+            </p>
+
+            <a href="https://www.youtube.com/watch?v=PElhjHIbyd0" target="_blank">
+            <button className='watch-lecture-btn-styles'>Watch Lecture</button>
+            </a>
+
+          </Card.Body>
+        </Card>
+
+
+        <Card className="featured-lecture-container">
+        <a href="https://www.youtube.com/watch?v=qh3JLgjEKkk&t=148s" target="_blank">
             <h1>Illinois, Chicago, and the Civil War</h1>
           </a>
-
           <img src={img2} className="lecture-img-2-styles"></img>
-        </div>
 
-        {/* <div className="featured-lecture-container">
-          <a href="https://www.youtube.com/watch?v=_7IKl0tDXEA&t=161s" target="_blank">
-            <h1>Calumet: The Region & The Anthropocene</h1>
-          </a>
+          <Card.Body>
+            <p className='lecture-description-styles'>
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat deleniti harum repellendus doloremque maxime sit ut, explicabo architecto rem commodi, a suscipit magnam, numquam odit asperiores ex fuga. Fugit, suscipit.
+            </p>
 
-          <img src={img3} className="lecture-img-2-styles"></img>
-        </div> */}
+            <a href="https://www.youtube.com/watch?v=qh3JLgjEKkk&t=148s" target="_blank">
+            <button className='watch-lecture-btn-styles'>Watch Lecture</button>
+            </a>
+
+          </Card.Body>
+        </Card>
+
 
         <Card className="featured-lecture-container">
           <a href="https://www.youtube.com/watch?v=_7IKl0tDXEA&t=161s" target="_blank">
