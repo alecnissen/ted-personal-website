1b097dad744d65f20d4089e7dd12d312fba7396d
alecnissen <alecnissen@yahoo.com>
2024-11-24 21:08:59 -0500

    saving work, trying to implement latest news images carousel

--- notes.js
+++ notes.js
@@ -1,1 +1,5 @@
 // fix the lecture content so they are cards just like the books 
+
+// *** 
+
+// complete the latest news section, then a mission state before the areas of expertise 
--- src/LatestNews.jsx
+++ src/LatestNews.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
+import img1 from './Assets/prof-ted-lecture-photo.png';
+import img2 from './Assets/prof-ted-lecture-photo-2.png';
+import img3 from './Assets/prof-ted-lecture-photo-3.png';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function LatestNews() {
   return (
+    <> 
     <div className='latest-news-content-wrapper'>
       <h1>Latest News</h1>
+
+
+      {/* <Carousel className='news-carousel-styles bootstrap-wrapper'>
+      <Carousel.Item>
+        
+        <Carousel.Caption className='news-carousel-title-subtitle-styles'>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel> */}
+
+
+
+
+
+
+
     </div>
+    </>
   )
 }
