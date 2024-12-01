13b8320979cc9ff20c23b11ad0be3a211e360323
alecnissen <alecnissen@yahoo.com>
2024-11-30 23:40:11 -0500

    saving work before implementing responsiveness to headers

--- src/LatestNews.jsx
+++ src/LatestNews.jsx
@@ -18,7 +18,7 @@ export default function LatestNews() {
        <div className='news-content-container'>
        <li> <a href='https://www.historians.org/perspectives-article/awards-prizes-and-honors-to-be-conferred-at-the-135th-annual-meeting-december-2021/' target='_blank'>Awarded Herbert Feis Award in Public History (2021)</a> </li>
         <li><a href='https://ncph.org/history-at-work/reflecting-on-the-founders-award/' target='_blank'>Winner of the Founders Award (2021)</a></li>
-        <li><a href='https://classicchicagomagazine.com/celebrating-chicago-the-queen-of-the-lakes/' target='_blank'>Celebrating Chicago, the Queen of the Lakes</a></li>
+        <li><a href='https://classicchicagomagazine.com/celebrating-chicago-the-queen-of-the-lakes/' target='_blank'>Celebrating Chicago, the Queen of the Lakes (2021)</a></li>
         <li><a href='https://jobs.luc.edu/history/news/2019stories/archive/proftheodorekaramanskihonoredwithlifetimeachievementaward.shtml' target='_blank'>Prof. Theodore Karamanski Honored with Lifetime Achievement Award (2019)</a></li>
         </div>
       
