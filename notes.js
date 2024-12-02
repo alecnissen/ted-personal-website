26cfdc0b26860999f3cf9e325e309f8bd7f07c7a
alecnissen <alecnissen@yahoo.com>
2024-12-01 21:59:54 -0500

    SAVE CHECKPOINT, started implementing header responsiveness and changed the header to a fixed header, will begin trying to use an offcanvas

--- notes.js
+++ notes.js
@@ -5,3 +5,15 @@
 // complete the latest news section, then a mission state before the areas of expertise 
 
 // get bullet points and text to line up ok
+
+
+// *** 
+
+// responsiveness for the header 
+
+// offcanvas vs regular column nav links ? 
+
+// bootstrap over riding previous styles? fix one section at a time *** 
+
+// fixed header *** 
+// get offcanvas working
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { useState } from 'react';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 export default function Header() {
   return (
@@ -11,6 +15,26 @@ export default function Header() {
         <button>Gallery</button>
         <button>Contact</button>
     </div>
+
+
+    {/* <Button variant="primary" className="d-lg-none" >
+        Launch
+      </Button>
+
+      <Alert variant="info" className="d-none d-lg-block">
+        Resize your browser to show the responsive offcanvas toggle.
+      </Alert>
+
+      <Offcanvas responsive="lg">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Responsive offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <p className="mb-0">
+            This is content within an <code>.offcanvas-lg</code>.
+          </p>
+        </Offcanvas.Body>
+      </Offcanvas> */}
 
 
     </div>
