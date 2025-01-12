6bd6c69ba309c32fb12f186cbc4689b782d9c666
alecnissen <alecnissen@yahoo.com>
2025-01-11 22:38:35 -0500

    added description to every link either book, book review or article, changed find button to say learn more

--- src/Publications.jsx
+++ src/Publications.jsx
@@ -19,7 +19,7 @@ export default function Publications() {
           
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/7/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/7/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -33,7 +33,7 @@ export default function Publications() {
             
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/10/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/10/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -48,7 +48,7 @@ export default function Publications() {
           
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/13/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/13/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -63,7 +63,7 @@ export default function Publications() {
          
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/22/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/22/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -72,12 +72,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Settler Colonial Strategies and Indigenous Resistance on the Great Lakes Lumber Frontier</h1>
+            <h1>Settler Colonial Strategies and Indigenous Resistance on the Great Lakes Lumber Frontier (Book Review)</h1>
 
             
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/61/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/61/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -94,7 +94,7 @@ export default function Publications() {
          
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/70/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/70/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -104,12 +104,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>The Beginning of Public History Ethics in the USA</h1>
+            <h1>The Beginning of Public History Ethics in the USA (Article)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/72/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/72/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -119,12 +119,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>What Price History: Politics, Commercialism, and Urban Preservation</h1>
+            <h1>What Price History: Politics, Commercialism, and Urban Preservation (Article)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/history_facpubs/69/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/history_facpubs/69/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -133,12 +133,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Fur Trade and Exploration: The Opening of the Far Northwest</h1>
+            <h1>Fur Trade and Exploration: The Opening of the Far Northwest (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/37/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/37/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -147,12 +147,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>One Hundred Years: A History of Roofing in America</h1>
+            <h1>One Hundred Years: A History of Roofing in America (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/38/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/38/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -161,12 +161,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Deep Woods Frontier: A History of Logging in Northern Michigan</h1>
+            <h1>Deep Woods Frontier: A History of Logging in Northern Michigan (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/39/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/39/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -175,12 +175,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Schooner Passage: Sailing Ships and the Lake Michigan Frontier</h1>
+            <h1>Schooner Passage: Sailing Ships and the Lake Michigan Frontier (Book)</h1>
 
             
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/40/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/40/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -190,12 +190,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Maritime Chicago</h1>
+            <h1>Maritime Chicago (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/41/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/41/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -204,12 +204,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Rally 'round the Flag : Chicago and the Civil War</h1>
+            <h1>Rally 'round the Flag : Chicago and the Civil War (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/220/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/220/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -218,12 +218,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Mastering the Inland Seas: How Lighthouses, Navigational Aids, and Harbors Transformed the Great Lakes and America</h1>
+            <h1>Mastering the Inland Seas: How Lighthouses, Navigational Aids, and Harbors Transformed the Great Lakes and America (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/219/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/219/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
@@ -232,12 +232,12 @@ export default function Publications() {
         <Card className="publications-card">
           {/* <img src={book1Img}></img> */}
           <Card.Body id="publications-card-body">
-            <h1>Blackbird's Song:Andrew J. Blackbird and the Odawa People</h1>
+            <h1>Blackbird's Song:Andrew J. Blackbird and the Odawa People (Book)</h1>
 
         
 
             <div className='publications-button-container'> 
-            <button><a href='https://ecommons.luc.edu/facultybooks/222/' target='_blank'>Find</a></button>
+            <button><a href='https://ecommons.luc.edu/facultybooks/222/' target='_blank'>Learn More</a></button>
             </div>
 
           </Card.Body>
