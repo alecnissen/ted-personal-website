7e7809473ab70ce75ec69643876fc1f94bc0b4b9
alecnissen <alecnissen@yahoo.com>
2025-01-11 22:21:06 -0500

    added publications content

--- src/Publications.jsx
+++ src/Publications.jsx
@@ -8,7 +8,7 @@ export default function Publications() {
     <> 
     <Header></Header>
     <div className='publications-page-content-wrapper'>
-      <h1 className='publications-header-text'>Publications & Lectures</h1>
+      <h1 className='publications-header-text'>Explore My Work</h1>
 
         <div className='publications-page-content-container'> 
         <Card className="publications-card">
@@ -16,10 +16,7 @@ export default function Publications() {
           <Card.Body id="publications-card-body">
             <h1>Museums, Monuments, and National Parks: Toward a New Genealogy of Public History (Book Review)</h1>
 
-            {/* <p>
-              Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p> */}
+          
 
             <div className='publications-button-container'> 
             <button><a href='https://ecommons.luc.edu/history_facpubs/7/' target='_blank'>Find</a></button>
@@ -33,10 +30,7 @@ export default function Publications() {
           <Card.Body id="publications-card-body">
             <h1>History, Memory, and Historic Districts in Chicago (Article)</h1>
 
-            {/* <p>
-              Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p> */}
+            
 
             <div className='publications-button-container'> 
             <button><a href='https://ecommons.luc.edu/history_facpubs/10/' target='_blank'>Find</a></button>
@@ -51,10 +45,7 @@ export default function Publications() {
           <Card.Body id="publications-card-body">
             <h1>A Catholic History of the Heartland: The Rise and Fall of Mid-America: A Historical Review (Article)</h1>
 
-            {/* <p>
-              Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p> */}
+          
 
             <div className='publications-button-container'> 
             <button><a href='https://ecommons.luc.edu/history_facpubs/13/' target='_blank'>Find</a></button>
@@ -69,10 +60,7 @@ export default function Publications() {
           <Card.Body id="publications-card-body">
             <h1>Monuments to a Lost Nation (Article)</h1>
 
-            {/* <p>
-              Some quick example text to build on the card title and make up the bulk of the card's
-              content.
-            </p> */}
+         
 
             <div className='publications-button-container'> 
             <button><a href='https://ecommons.luc.edu/history_facpubs/22/' target='_blank'>Find</a></button>
@@ -81,10 +69,189 @@ export default function Publications() {
           </Card.Body>
         </Card>
 
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Settler Colonial Strategies and Indigenous Resistance on the Great Lakes Lumber Frontier</h1>
+
+            
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/history_facpubs/61/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+   
+
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>A Midwesterner's Reflections on Teaching Public History in China (Article)</h1>
+
+         
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/history_facpubs/70/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>The Beginning of Public History Ethics in the USA</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/history_facpubs/72/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>What Price History: Politics, Commercialism, and Urban Preservation</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/history_facpubs/69/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Fur Trade and Exploration: The Opening of the Far Northwest</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/37/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>One Hundred Years: A History of Roofing in America</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/38/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Deep Woods Frontier: A History of Logging in Northern Michigan</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/39/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Schooner Passage: Sailing Ships and the Lake Michigan Frontier</h1>
+
+            
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/40/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Maritime Chicago</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/41/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Rally 'round the Flag : Chicago and the Civil War</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/220/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Mastering the Inland Seas: How Lighthouses, Navigational Aids, and Harbors Transformed the Great Lakes and America</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/219/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        <Card className="publications-card">
+          {/* <img src={book1Img}></img> */}
+          <Card.Body id="publications-card-body">
+            <h1>Blackbird's Song:Andrew J. Blackbird and the Odawa People</h1>
+
+        
+
+            <div className='publications-button-container'> 
+            <button><a href='https://ecommons.luc.edu/facultybooks/222/' target='_blank'>Find</a></button>
+            </div>
+
+          </Card.Body>
+        </Card>
+
+        
+
+
 
         </div>
 
     </div>
+
+    <Footer></Footer>
     
     </>
   )
