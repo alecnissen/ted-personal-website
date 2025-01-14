bab72916d69fddd1671a06dc6667f705101d58d4
alecnissen <alecnissen@yahoo.com>
2025-01-13 21:46:15 -0500

    added second row of pictures, saving work and will add third row of pictures

--- src/GalleryPage.jsx
+++ src/GalleryPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Header from './Header'
+import Footer from './Footer'
 import tedImg1 from './Assets/Northern.Quebec.1981.jpg'
 import tedImg2 from './Assets/Salmon.Headwaters.Yukon.River.1981.jpg'
 import tedImg3 from './Assets/Ted & Glenn.Yukon.River.1981 (1).jpg'
 import tedImg4 from './Assets/Public.Hist.Maritime.Museum.jpg'
 import tedImg5 from './Assets/aebe1d08-8741-46ba-85a7-201ba8781dd8.jpg'
 import tedImg6 from './Assets/Cascades Climb.jpg'
+import tedImg7 from './Assets/New_Karamanski_picture1 (2).jpg'
+import tedImg8 from './Assets/Image-1.jpeg'
+import tedImg9 from './Assets/IMG_0615.jpeg'
+import tedImg10 from './Assets/IMG_3217.jpeg'
 
 export default function GalleryPage() {
   return (
@@ -13,18 +18,26 @@ export default function GalleryPage() {
     <Header></Header>
     <div className='gallery-page-content-wrapper'>
       
-        <div className='gallery-page-img-container'> 
+        <div className='gallery-page-img-container-1'> 
             
             <img src={tedImg1}></img>
             <img src={tedImg2}></img>
             <img src={tedImg3}></img>
             <img src={tedImg6}></img>
-            {/* <img src={tedImg4}></img>
-            <img src={tedImg5}></img> */}
+        </div>
+
+        <div className='gallery-page-img-container-2'> 
+            
+            <img src={tedImg5}></img>
+            <img src={tedImg8}></img>
+            <img src={tedImg9}></img>
+            {/* <img src={tedImg10}></img> */}
         </div>
 
 
     </div>
+
+<Footer></Footer>
     </>
   )
 }
