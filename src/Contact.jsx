025d666f42ddcbdfc2053830a3cf748ea6b42c08
alecnissen <alecnissen@yahoo.com>
2025-01-18 19:55:38 -0500

    testing form submission

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -10,21 +10,22 @@ export default function Contact() {
       <h1>Connect With Me!</h1>
 
 
-    <form className='contact-us-form' name='contact-form' netlify> 
+    <form className='contact-us-form' name='contact-form' method='POST' data-netlify="true"> 
+    <input type="hidden" name="form-name" value="contact-form" /> 
       
         <div className='fname-lname-email-container'> 
             <div className='fname-container'>
-                <label>First Name *</label>
-                <input type='text'></input>
+                <label for='first-name-input'>First Name *</label>
+                <input type='text' name='first-name' id='first-name-input'></input>
             </div>
             <div className='lname-container'>
-                <label>Last Name *</label>
-                <input type='text'></input>
+                <label for='last-name-input'>Last Name *</label>
+                <input type='text' name='last-name' id='last-name-input'></input>
             </div>
 
             <div className='email-container'> 
-            <label>E-Mail *</label>
-            <input type='email'></input>
+            <label for='email-input'>E-Mail *</label>
+            <input type='email' name='email' id='email-input'></input>
             </div>
 
             
@@ -34,19 +35,19 @@ export default function Contact() {
         <div className='phone-container-date-time-container'>
 
             <div className='phone-container'>
-            <label>Phone Number *</label>
-            <input type='phone'></input>
+            <label for='phone-number-input'>Phone Number *</label>
+            <input type='phone' name='phone' id='phone-number-input'></input>
             </div>
 
 
           <div className='event-date-container'> 
-            <label>Event Date *</label> 
-            <input type='text'></input>
+            <label for='event-date-input'>Event Date *</label> 
+            <input type='text' name='event-date' id='event-date-input'></input>
           </div>
 
           <div className='event-time-container'> 
-            <label>Event Time *</label>
-            <input type='text'></input>
+            <label for='event-time-input'>Event Time *</label>
+            <input type='text' name='event-time' id='event-time-input'></input>
           </div>
 
 
@@ -59,24 +60,24 @@ export default function Contact() {
         <div className='hours-venue-name-address-wrapper'>
         <div className='hours-needed-wrapper'>
           <div className='hours-needed-container'> 
-          <label>Hours Needed *</label>
-          <input type='text'></input>
+          <label for='hours-needed-input'>Hours Needed *</label>
+          <input type='text' name='hours-needed' id='hours-needed-input'></input>
 
           </div>
         </div>
 
         <div className='venue-name-wrapper'>
           <div className='venue-name-container'> 
-          <label>Venue Name *</label>
-          <input type='text'></input>
+          <label for='venue-name-input'>Venue Name *</label>
+          <input type='text' name='venue-name' id='venue-name-input'></input>
           </div>
         </div>
 
 
       
           <div className='venue-address-container'> 
-            <label>Venue Address *</label>
-            <input type='text'></input>
+            <label for='address-input'>Venue Address *</label>
+            <input type='text' name='address' id='address-input'></input>
           </div>
 
 
@@ -86,8 +87,8 @@ export default function Contact() {
 
     <div className='message-wrapper'>
       <div className='message-container'>
-        <label>Your Message</label>
-        <textarea></textarea>
+        <label for='message-box'>Your Message</label>
+        <textarea name='message' type='text' id='message-box'></textarea>
       </div>
 
     <div className='submit-btn-container'>
