7bf9122c8f399e7c634587101b70a56f750e1ba4
alecnissen <alecnissen@yahoo.com>
2025-01-19 17:42:58 -0500

    testing

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -25,7 +25,7 @@ export default function Contact() {
 
 
 
-    <form className='contact-us-form' name='contact-form' method='POST' data-netlify="true" onSubmit={handleSubmit}> 
+    <form className='contact-us-form' name='contact-form' method='POST' data-netlify="true"> 
     <input type="hidden" name="form-name" value="contact-form" /> 
       
         <div className='fname-lname-email-container'> 
