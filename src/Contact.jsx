5913b69c3f10ca69861fe1f5e5a3ef209e309e39
alecnissen <alecnissen@yahoo.com>
2025-01-19 17:50:49 -0500

    removed prevent default in the handleSubmit

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -8,7 +8,6 @@ export default function Contact() {
   const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = (e) => { 
-    e.preventDefault();
     setIsSubmitted(true);
   };
 
@@ -25,7 +24,7 @@ export default function Contact() {
 
 
 
-    <form className='contact-us-form' name='contact-form' method='POST' data-netlify="true"> 
+    <form className='contact-us-form' name='contact-form' method='POST' data-netlify="true" onSubmit={handleSubmit}> 
     <input type="hidden" name="form-name" value="contact-form" /> 
       
         <div className='fname-lname-email-container'> 
